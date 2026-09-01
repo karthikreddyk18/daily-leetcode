0c4bea80ced3015ced9d688ce3251793785a21f4
Karthik Reddy Kanchanakuntla <b23it011@kitsw.ac.in>
2026-09-01 21:15:51 +0530

    Time: 8 ms (99.6%), Space: 103.9 MB (5.34%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -1,21 +1,36 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
-        Arrays.sort(nums);
-        int left = 0;
-        long actualSum = 0;
-        int maxFreq = 0;
-        for (int right = 0; right < nums.length; right++) {
-            actualSum += nums[right];
-
-            // If operations needed exceed k, shrink window from left
-            while ((long) nums[right] * (right - left + 1) - actualSum > k) {
-                actualSum -= nums[left];
-                left++;
-            }
-
-            maxFreq = Math.max(maxFreq, right - left + 1);
+        int maxVal = 0;
+        for (int num : nums) {
+            if (num > maxVal) maxVal = num;
+        }
+        
+      
+        int[] counts = new int[maxVal + 1];
+        for (int num : nums) {
+            counts[num]++;
         }
 
-        return maxFreq;
+        int idx = 0;
+        for(int i=0; i<= maxVal; i++){
+            while(counts[i] > 0){
+                nums[idx++] = i;
+                counts[i]--;
+            }
+        }
+
+        int lef = 0;
+        long currWin =0;
+        
+        for(int rig=0; rig < nums.length; rig++){
+            currWin += nums[rig];
+
+            if((long) nums[rig] * (rig - lef + 1) - currWin > k){
+                currWin -= nums[lef];
+                lef++;
+            }
+        }
+        return nums.length - lef;
+        
     }
 }
