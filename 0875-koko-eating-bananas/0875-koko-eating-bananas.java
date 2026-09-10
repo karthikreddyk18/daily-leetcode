ebfab97821b883fde05c6dff3dea7ed47d859ba5
Karthik Reddy Kanchanakuntla <b23it011@kitsw.ac.in>
2026-09-10 09:15:16 +0530

    Time: 6 ms (99.76%), Space: 47.9 MB (57.7%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,33 +1,26 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int l=1;
-        int r=0;
-        for(int i:piles){
-            if(i>r){
-                r=i;
-            }
-        }
-        int ans=r;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(canEat(piles,mid,h)){
-                ans=mid;
-                r=mid-1;
-            }else{
-                l=mid+1;
-            }
-        }
-        return ans;
-    }
-    private boolean canEat(int[] piles, int k, int h) {
-        long totalHours = 0;
+        int left = 1, right = 0;
         for (int pile : piles) {
-            // Equivalent to Math.ceil((double) pile / k)
-            totalHours += (pile + k - 1L) / k;
-            if (totalHours > h) {
-                return false; // Early pruning
+            right = Math.max(right, pile);
+        }
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canEat(piles, h, mid)) {
+                right = mid; // try smaller speed
+            } else {
+                left = mid + 1; // need bigger speed
             }
         }
-        return totalHours <= h;
+        return left;
+    }
+    
+    private boolean canEat(int[] piles, int h, int speed) {
+        int hours = 0;
+        for (int pile : piles) {
+            hours += (pile + speed - 1) / speed; // ceil(pile/speed)
+        }
+        return hours <= h;
     }
 }
